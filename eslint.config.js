ccc04f5e157ff22d0c483cf7b43adbbb1c4097a1
Mathis <mathis.quemener@gmail.com>
2025-06-19 19:45:42 +0200

    🔧 refactor eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -20,11 +19,5 @@ export default defineConfig([
   {
     files: ["**/*.vue"],
     languageOptions: { parserOptions: { parser: tseslint.parser } },
-  },
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
   },
 ]);
