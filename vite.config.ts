ef899d8fbdb544ba4d848a9a37a4d8b6f14e4dbf
Mathis <mathis.quemener@gmail.com>
2025-06-27 17:47:59 +0200

    ⬇️ Change vertsion integrate Tailwind CSS into the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
   base: './',
   resolve: {
     alias: {
